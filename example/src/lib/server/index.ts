713fb98e406eed7bb4b06aa060d806184e88d96b
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2026-05-07 08:46:17 +0100

    Remove async hook

--- example/src/lib/server/index.ts
+++ example/src/lib/server/index.ts
@@ -1,9 +1,3 @@
 export { EchoDO } from './EchoDO';
 export { BotWorkflow } from './BotWorkflow';
 export { default } from './devHandler';
-
-import async_hooks from 'node:async_hooks';
-const asyncHook = async_hooks.createHook({
-  init(asyncId, type, triggerAsyncId, resource) { },
-  destroy(asyncId) { },
-});
